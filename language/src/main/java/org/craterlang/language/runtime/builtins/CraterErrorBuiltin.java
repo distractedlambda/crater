b2029485e65ef757ae62f55e43ce2b0696eafefa
Lucas Myers <lucascharlesmyers@gmail.com>
2022-12-03 09:35:51 -0500

    WIP

--- language/src/main/java/org/craterlang/language/runtime/builtins/CraterErrorBuiltin.java
+++ language/src/main/java/org/craterlang/language/runtime/builtins/CraterErrorBuiltin.java
@@ -1,12 +1,21 @@
 package org.craterlang.language.runtime.builtins;
 
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.GenerateUncached;
+import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.nodes.EncapsulatingNodeReference;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.profiles.BranchProfile;
 import com.oracle.truffle.api.profiles.ConditionProfile;
 import com.oracle.truffle.api.profiles.IntValueProfile;
+import com.oracle.truffle.api.source.SourceSection;
+import com.oracle.truffle.api.strings.TruffleString;
+import com.oracle.truffle.api.strings.TruffleStringBuilder;
+import org.craterlang.language.CraterLanguage;
 import org.craterlang.language.CraterNode;
 import org.craterlang.language.nodes.CraterForceIntoIntegerNode;
 import org.craterlang.language.runtime.CraterBuiltin;
@@ -16,11 +25,11 @@ import static org.craterlang.language.CraterTypeSystem.isNil;
 
 public final class CraterErrorBuiltin extends CraterBuiltin {
     @Override public BodyNode createBodyNode() {
-        return null;
+        return CraterErrorBuiltinFactory.ImplNodeGen.create();
     }
 
     @Override public Object callUncached(Object[] arguments) {
-        return null;
+        return CraterErrorBuiltinFactory.ImplNodeGen.getUncached().execute(arguments);
     }
 
     @GenerateUncached
@@ -64,29 +73,165 @@ public final class CraterErrorBuiltin extends CraterBuiltin {
     static abstract class AddLocationToMessageNode extends CraterNode {
         abstract Object execute(Object message, long level);
 
-        @TruffleBoundary
-        @Specialization(guards = "level > 0")
-        byte[] doAddLocation(byte[] message, long level) {
-            var skipFrames = level - 1;
-
-            if (skipFrames > Integer.MAX_VALUE) {
+        @Specialization(guards = {"level > 0", "level < 2147483647"})
+        TruffleString doAddLocation(
+            TruffleString message,
+            long level,
+            @Cached GetSourceLocationNode getSourceLocationNode,
+            @Cached BranchProfile nullSourceLocationProfile,
+            @Cached BuildMessageWithLocationNode buildMessageWithLocationNode
+        ) {
+            var location = getSourceLocationNode.execute((int) (level - 2));
+            if (location == null) {
+                nullSourceLocationProfile.enter();
                 return message;
             }
-
-            throw new UnsupportedOperationException("TODO");
-
-            // var location = Truffle.getRuntime().iterateFrames(
-            //     frame -> {
-            //         frame.
-
-            //     },
-            //     (int) (skipFrames)
-            // );
+            else {
+                return buildMessageWithLocationNode.execute(message, location);
+            }
         }
 
         @Fallback
         Object doPassThrough(Object message, long level) {
             return message;
+        }
+    }
+
+    @GenerateUncached
+    static abstract class BuildMessageWithLocationNode extends CraterNode {
+        abstract TruffleString execute(TruffleString message, SourceLocation sourceLocation);
+
+        @Specialization
+        TruffleString doExecute(
+            TruffleString message,
+            SourceLocation sourceLocation,
+            @Cached TruffleStringBuilder.AppendStringNode appendFileNode,
+            @Cached TruffleStringBuilder.AppendByteNode appendFileLineSeparatorNode,
+            @Cached TruffleStringBuilder.AppendIntNumberNode appendLineNumberNode,
+            @Cached TruffleStringBuilder.AppendByteNode appendLineColumnSeparatorNode,
+            @Cached TruffleStringBuilder.AppendIntNumberNode appendColumnNumberNode,
+            @Cached TruffleStringBuilder.AppendByteNode appendLocationMessageSeparatorNode,
+            @Cached TruffleStringBuilder.AppendStringNode appendMessageNode,
+            @Cached TruffleStringBuilder.ToStringNode builderToStringNode
+        ) {
+            var builder = TruffleStringBuilder.create(TruffleString.Encoding.BYTES);
+            appendFileNode.execute(builder, sourceLocation.file);
+            appendFileLineSeparatorNode.execute(builder, (byte) ':');
+            appendLineNumberNode.execute(builder, sourceLocation.line);
+            appendLineColumnSeparatorNode.execute(builder, (byte) ':');
+            appendColumnNumberNode.execute(builder, sourceLocation.column);
+            appendLocationMessageSeparatorNode.execute(builder, (byte) ' ');
+            appendMessageNode.execute(builder, message);
+            return builderToStringNode.execute(builder);
+        }
+    }
+
+    @GenerateUncached
+    @ImportStatic(SourceLocation.class)
+    static abstract class GetSourceLocationNode extends CraterNode {
+        abstract SourceLocation execute(int skipFrames);
+
+        @Specialization(guards = "skipFrames < 0")
+        SourceLocation doSelf(
+            int skipFrames,
+            @Cached(value = "getSourceLocation()", allowUncached = true) SourceLocation sourceLocation
+        ) {
+            return sourceLocation;
+        }
+
+        SourceLocation getSourceLocation() {
+            Node target;
+
+            if (isAdoptable()) {
+                target = this;
+            }
+            else {
+                target = EncapsulatingNodeReference.getCurrent().get();
+                if (target == null) {
+                    return null;
+                }
+            }
+
+            return SourceLocation.ofNode(target);
+        }
+
+        @Fallback
+        SourceLocation doCaller(
+            int skipFrames,
+            @Cached GetCallSiteSourceLocationNode getCallSiteSourceLocationNode
+        ) {
+            return getCallSiteSourceLocationNode.execute(getCallSite(skipFrames));
+        }
+
+        @TruffleBoundary
+        private static Node getCallSite(int skipFrames) {
+            var callSite = Truffle.getRuntime().iterateFrames(
+                frame -> {
+                    var callNode = frame.getCallNode();
+                    if (callNode == null) {
+                        return UNKNOWN_CALL_SITE;
+                    }
+                    else {
+                        return callNode;
+                    }
+                },
+                skipFrames
+            );
+
+            if (callSite == UNKNOWN_CALL_SITE) {
+                return null;
+            }
+            else {
+                return (Node) callSite;
+            }
+        }
+
+        private static final Object UNKNOWN_CALL_SITE = new Object();
+    }
+
+    @GenerateUncached
+    @ImportStatic(SourceLocation.class)
+    static abstract class GetCallSiteSourceLocationNode extends CraterNode {
+        abstract SourceLocation execute(Node callSite);
+
+        @Specialization(guards = "callSite == null")
+        SourceLocation doNull(Node callSite) {
+            return null;
+        }
+
+        @Specialization(guards = {"callSite != null", "callSite == cachedCallSite"})
+        SourceLocation doConstant(
+            Node callSite,
+            @Cached(value = "callSite", adopt = false) Node cachedCallSite,
+            @Cached("ofNode(cachedCallSite)") SourceLocation sourceLocation
+        ) {
+            return sourceLocation;
+        }
+
+        @TruffleBoundary
+        @Specialization(replaces = {"doNull", "doConstant"})
+        SourceLocation doDynamic(Node node) {
+            return SourceLocation.ofNode(node);
+        }
+    }
+
+    record SourceLocation(TruffleString file, int line, int column) {
+        static SourceLocation ofSourceSection(SourceSection sourceSection) {
+            return new SourceLocation(
+                CraterLanguage.get(null).getLiteralString(sourceSection.getSource().getPath()),
+                sourceSection.getStartLine(),
+                sourceSection.getStartColumn()
+            );
+        }
+
+        static SourceLocation ofNode(Node node) {
+            var sourceSection = node.getEncapsulatingSourceSection();
+            if (sourceSection == null) {
+                return null;
+            }
+            else {
+                return ofSourceSection(sourceSection);
+            }
         }
     }
 }
