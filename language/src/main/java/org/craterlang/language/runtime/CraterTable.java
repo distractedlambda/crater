2812bed9c32b0796e67640c45519ef59f55be740
Lucas Myers <lucascharlesmyers@gmail.com>
2022-12-03 15:29:45 -0500

    WIP

--- language/src/main/java/org/craterlang/language/runtime/CraterTable.java
+++ language/src/main/java/org/craterlang/language/runtime/CraterTable.java
@@ -6,253 +6,181 @@ import com.oracle.truffle.api.dsl.GenerateUncached;
 import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.nodes.UnexpectedResultException;
-import com.oracle.truffle.api.strings.TruffleString;
+import com.oracle.truffle.api.object.DynamicObject;
+import com.oracle.truffle.api.object.DynamicObjectLibrary;
+import com.oracle.truffle.api.object.HiddenKey;
+import com.oracle.truffle.api.object.Shape;
+import com.oracle.truffle.api.profiles.ConditionProfile;
 import org.craterlang.language.CraterNode;
 
-import static com.oracle.truffle.api.CompilerDirectives.castExact;
-import static java.lang.Double.doubleToRawLongBits;
-import static java.lang.System.identityHashCode;
+import static com.oracle.truffle.api.CompilerDirectives.shouldNotReachHere;
 
-public final class CraterTable implements TruffleObject {
-    private int header = 0;
-    private Object metatableOrKeys = CraterNil.getInstance();
-    private Object mapValues = null;
-    private Object optimizedSequenceValues = null;
-
-    boolean hasMap() {
-        return metatableOrKeys.getClass() == Object[].class;
+public final class CraterTable extends DynamicObject implements TruffleObject {
+    public CraterTable(Shape shape) {
+        super(shape);
     }
 
-    boolean hasOptimizedSequence() {
-        return optimizedSequenceValues != null;
+    boolean hasOptimizedArray(DynamicObjectLibrary tables) {
+        return (tables.getShapeFlags(this) & OPTIMIZED_ARRAY_FLAG) != 0;
     }
 
-    boolean sequenceOptimizationIsDisabled() {
-        return (header & 0x1) != 0;
+    Object getOptimizedArrayStorage(DynamicObjectLibrary tables) {
+        return tables.getOrDefault(this, OPTIMIZED_ARRAY_STORAGE_KEY, null);
     }
 
-    boolean keysAreInterned() {
-        return (header & 0x2) != 0;
+    long getOptimizedArrayLength(DynamicObjectLibrary tables) {
+        try {
+            return tables.getLongOrDefault(this, OPTIMIZED_ARRAY_LENGTH_KEY, null);
+        }
+        catch (UnexpectedResultException exception) {
+            throw shouldNotReachHere(exception);
+        }
     }
 
-    public Object[] getKeys() {
-        return castExact(metatableOrKeys, Object[].class);
-    }
+    private static final int OPTIMIZED_ARRAY_FLAG = 0x01;
 
-    int getLength() {
-        return header >>> HEADER_LENGTH_SHIFT;
-    }
-
-    static final int HEADER_LENGTH_SHIFT = 2;
-    static final int INVALID_SEQUENCE_LENGTH = -1 >>> HEADER_LENGTH_SHIFT;
+    private static final HiddenKey LENGTH_KEY = new HiddenKey("length");
+    private static final HiddenKey OPTIMIZED_ARRAY_STORAGE_KEY = new HiddenKey("optimizedSequenceStorage");
+    private static final HiddenKey OPTIMIZED_ARRAY_LENGTH_KEY = new HiddenKey("optimizedSequenceLength");
 
     @GenerateUncached
     public static abstract class GetMetatableNode extends CraterNode {
         public abstract Object execute(CraterTable table);
 
-        @Specialization
-        Object doExecute(CraterTable table, @Cached ExtractNode extractNode) {
-            return extractNode.execute(table);
+        @Specialization(guards = "table.getShape() == cachedShape")
+        Object doConstantShape(CraterTable table, @Cached("table.getShape()") Shape cachedShape) {
+            return cachedShape.getDynamicType();
         }
 
-        @GenerateUncached
-        static abstract class ExtractNode extends CraterNode {
-            abstract Object execute(Object metatableOrKeys);
+        @Specialization(replaces = "doConstantShape")
+        Object doDynamicShape(CraterTable table) {
+            return table.getShape().getDynamicType();
+        }
+    }
 
-            @Specialization
-            Object doKeys(Object[] keys) {
-                return keys[0];
+    @GenerateUncached
+    public static abstract class SetMetatableNode extends CraterNode {
+        public abstract void execute(CraterTable table, Object metatable);
+
+        @Specialization(limit = "3")
+        void doExecute(CraterTable table, Object metatable, @CachedLibrary("table") DynamicObjectLibrary tables) {
+            tables.setDynamicType(table, metatable);
+        }
+    }
+
+    @GenerateUncached
+    public static abstract class RawLengthNode extends CraterNode {
+        public abstract long execute(CraterTable table);
+
+        @Specialization(limit = "3")
+        long doExecute(CraterTable table, @CachedLibrary("table") DynamicObjectLibrary tables) {
+            try {
+                return tables.getLongOrDefault(table, LENGTH_KEY, 0);
             }
-
-            @Fallback
-            Object doMetatable(Object metatable) {
-                return metatable;
+            catch (UnexpectedResultException exception) {
+                throw shouldNotReachHere(exception);
             }
         }
     }
 
     @GenerateUncached
-    @ImportStatic(Double.class)
-    public static abstract class RawIndexNode extends CraterNode {
+    public static abstract class RawGetNode extends CraterNode {
         public abstract Object execute(CraterTable table, Object key);
 
         @Specialization
-        CraterNil doNilKey(CraterTable table, CraterNil key) {
-            return CraterNil.getInstance();
-        }
-
-        @Specialization(guards = "isNaN(key)")
-        CraterNil doNanKey(CraterTable table, double key) {
-            return CraterNil.getInstance();
-        }
-
-        @Fallback
-        Object doNormalizedKey(
+        Object doExecute(
             CraterTable table,
             Object key,
             @Cached NormalizeKeyNode normalizeKeyNode,
-            @Cached RawIndexWithNormalizedKeyNode withNormalizedKeyNode
+            @Cached RawGetWithNormalizedKeyNode rawGetWithNormalizedKeyNode
         ) {
-            return withNormalizedKeyNode.execute(table, normalizeKeyNode.execute(key));
+            return rawGetWithNormalizedKeyNode.execute(table, normalizeKeyNode.execute(key));
         }
     }
 
     @GenerateUncached
-    static abstract class RawIndexWithNormalizedKeyNode extends CraterNode {
+    static abstract class RawGetWithNormalizedKeyNode extends CraterNode {
         abstract Object execute(CraterTable table, Object normalizedKey);
 
-        @Specialization(guards = {"table.hasOptimizedSequence()", "key > 0", "key <= table.getLength()"})
-        Object doOptimizedSequence(
+        @Specialization(limit = "3")
+        Object doExecute(
             CraterTable table,
-            long key,
-            @Cached IndexVectorNode optimizedSequenceNode
+            Object key,
+            @CachedLibrary("table") DynamicObjectLibrary tables,
+            @Cached RawGetWithLibraryNode rawGetWithLibraryNode
         ) {
-            return optimizedSequenceNode.execute(table.optimizedSequenceValues, (int) key);
+            return rawGetWithLibraryNode.execute(table, key, tables);
+        }
+    }
+
+    @GenerateUncached
+    static abstract class RawGetWithLibraryNode extends CraterNode {
+        abstract Object execute(CraterTable table, Object normalizedKey, DynamicObjectLibrary tables);
+
+        @Specialization(guards = {"table.hasOptimizedArray(tables)", "index > 0"})
+        Object doOptimizedArray(
+            CraterTable table,
+            long index,
+            DynamicObjectLibrary tables,
+            @Cached("createCountingProfile()") ConditionProfile inBoundsProfile,
+            @Cached ReadOptimizedArrayElementNode readOptimizedArrayElementNode
+        ) {
+            var length = table.getOptimizedArrayLength(tables);
+            if (inBoundsProfile.profile(index <= length)) {
+                var storage = table.getOptimizedArrayStorage(tables);
+                return readOptimizedArrayElementNode.execute(storage, index);
+            }
+            else {
+                return CraterNil.getInstance();
+            }
         }
 
         @Fallback
-        Object doMap(CraterTable table, Object key) {
-            throw new UnsupportedOperationException("TODO");
+        Object doOther(CraterTable table, Object key, DynamicObjectLibrary tables) {
+            return tables.getOrDefault(table, key, CraterNil.getInstance());
         }
     }
 
     @GenerateUncached
-    static abstract class IndexVectorNode extends CraterNode {
-        abstract Object execute(Object optimizedSequenceValues, int index);
+    static abstract class ReadOptimizedArrayElementNode extends CraterNode {
+        abstract Object execute(Object storage, long index);
 
         @Specialization
-        boolean doBooleans(boolean[] values, int index) {
-            return values[index];
+        boolean doBooleans(boolean[] storage, long index) {
+            return storage[(int) index];
         }
 
         @Specialization
-        long doLongs(long[] values, int index) {
-            return values[index];
+        long doLongs(long[] storage, long index) {
+            return storage[(int) index];
         }
 
         @Specialization
-        double doDoubles(double[] values, int index) {
-            return values[index];
+        double doDoubles(double[] storage, long index) {
+            return storage[(int) index];
         }
 
         @Specialization
-        Object doGeneric(Object[] values, int index) {
-            return values[index];
+        Object doObjects(Object[] storage, long index) {
+            return storage[(int) index];
         }
     }
 
     @GenerateUncached
-    static abstract class FindEntryNode extends CraterNode {
-        abstract int execute(CraterTable table, Object key);
-
-        @Specialization(guards = "!table.hasMap()")
-        int doEmpty(CraterTable table, Object key) {
-            return -1;
-        }
-
-        @Specialization(guards = "table.hasMap()")
-        int doBooleanKey(CraterTable table, boolean key) {
-
-        }
-    }
-
-    @GenerateUncached
-    static abstract class FindDynamicEntryNode extends CraterNode {
-        abstract int execute(Object[] keys, Object key);
-    }
-
-    // @GenerateUncached
-    // protected static abstract class RawIndexMapNode extends CraterNode {
-    //     protected abstract Object execute(CraterTable table, Object normalizedKey);
-
-    //     @Specialization(guards = "!table.hasMap()")
-    //     protected CraterNil doEmpty(CraterTable table, Object key) {
-    //         return CraterNil.getInstance();
-    //     }
-
-    //     @Fallback
-    //     protected Object doNonEmpty(
-    //         CraterTable table,
-    //         Object key,
-    //         @Cached KeyHashNode keyHashNode,
-    //         @Cached RawIndexNonEmptyMapNode rawIndexNonEmptyMapNode
-    //     ) {
-    //         return rawIndexNonEmptyMapNode.execute(table, key, keyHashNode.execute(key));
-    //     }
-    // }
-
-    // @GenerateUncached
-    // protected static abstract class RawIndexNonEmptyMapNode extends CraterNode {
-    //     protected abstract Object execute(CraterTable table, Object key, int hashCode);
-
-    //     @Specialization(guards = {"table.keysAreInterned()", "keys == cachedKeys"})
-    //     protected Object doInternedKeys(
-    //         CraterTable table,
-    //         @Cached(value = "table.getKeys()", dimensions = 1) Object[] cachedKeys,
-    //
-    //     )
-    // }
-
-    @GenerateUncached
+    @ImportStatic(CraterMath.class)
     static abstract class NormalizeKeyNode extends CraterNode {
         abstract Object execute(Object key);
 
-        @Specialization(rewriteOn = UnexpectedResultException.class)
-        long doDoubleWithExactLongValue(double key) throws UnexpectedResultException {
-            return CraterMath.expectExactLongValue(key);
-        }
-
-        @Specialization(replaces = "doDoubleWithExactLongValue")
-        Object doDouble(double key) {
-            return CraterMath.coerceExactLongValue(key);
+        @Specialization(guards = "hasExactLongValue(key)")
+        long doDoubleToLong(double key) {
+            return (long) key;
         }
 
         @Fallback
-        Object doOther(Object value) {
-            return value;
+        Object doPassThrough(Object key) {
+            return key;
         }
-    }
-
-    @GenerateUncached
-    static abstract class KeyHashNode extends CraterNode {
-        abstract int execute(Object key);
-
-        @Specialization
-        int doBoolean(boolean key) {
-            return key ? TRUE_HASH_CODE : FALSE_HASH_CODE;
-        }
-
-        @Specialization
-        int doLong(long key) {
-            return hash64(key);
-        }
-
-        @Specialization
-        int doDouble(double key) {
-            return hash64(doubleToRawLongBits(key));
-        }
-
-        @Fallback
-        int doIdentity(Object key) {
-            return identityHashCode(key);
-        }
-
-        private static int hash64(long bits) {
-            return scramble32((int) bits ^ (int) (bits >>> 32));
-        }
-
-        // https://github.com/skeeto/hash-prospector/issues/19
-        private static int scramble32(int x) {
-            x ^= x >>> 16;
-            x *= 0x21f0aaad;
-            x ^= x >>> 15;
-            x *= 0xd35a2d97;
-            x ^= x >>> 15;
-            return x;
-        }
-
-        private static final int FALSE_HASH_CODE = scramble32(0);
-        private static final int TRUE_HASH_CODE = scramble32(1);
     }
 }
