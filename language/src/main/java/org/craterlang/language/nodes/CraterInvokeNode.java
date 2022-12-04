9ed569c3f3299151840815595b1d42ee4bca870c
Lucas Myers <lucascharlesmyers@gmail.com>
2022-12-04 10:03:28 -0500

    WIP

--- language/src/main/java/org/craterlang/language/nodes/CraterInvokeNode.java
+++ language/src/main/java/org/craterlang/language/nodes/CraterInvokeNode.java
@@ -3,31 +3,76 @@ package org.craterlang.language.nodes;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.GenerateUncached;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.profiles.BranchProfile;
 import org.craterlang.language.CraterNode;
 import org.craterlang.language.runtime.CraterBuiltin;
 import org.craterlang.language.runtime.CraterClosure;
+
+import static com.oracle.truffle.api.CompilerDirectives.hasNextTier;
+import static com.oracle.truffle.api.nodes.LoopNode.reportLoopCount;
 
 @GenerateUncached
 public abstract class CraterInvokeNode extends CraterNode {
     public abstract Object execute(Object callee, Object continuationFrame, Object[] arguments);
 
     @Specialization
-    Object doClosure(
-        CraterClosure callee,
+    Object doExecute(
+        Object callee,
         Object continuationFrame,
         Object[] arguments,
-        @Cached CraterClosure.InvokeNode closureInvokeNode
+        @Cached DispatchNode dispatchNode,
+        @Cached BranchProfile tailCallProfile
     ) {
-        return closureInvokeNode.execute(callee, continuationFrame, arguments);
+        Object result;
+        var tailCallCount = 0L;
+
+        for (;;) {
+            try {
+                result = dispatchNode.execute(callee, continuationFrame, arguments);
+                break;
+            }
+            catch (CraterTailCallException exception) {
+                tailCallProfile.enter();
+
+                callee = exception.getCallee();
+                continuationFrame = exception.getContinuationFrame();
+                arguments = exception.getArguments();
+
+                if (hasNextTier()) {
+                    tailCallCount++;
+                }
+            }
+        }
+
+        if (tailCallCount != 0) {
+            reportLoopCount(this, tailCallCount > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) tailCallCount);
+        }
+
+        return result;
     }
 
-    @Specialization
-    Object doBuiltin(
-        CraterBuiltin callee,
-        Object continuationFrame,
-        Object[] arguments,
-        @Cached CraterBuiltin.InvokeNode builtinInvokeNode
-    ) {
-        return builtinInvokeNode.execute(callee, continuationFrame, arguments);
+    @GenerateUncached
+    static abstract class DispatchNode extends CraterNode {
+        abstract Object execute(Object callee, Object continuationFrame, Object[] arguments);
+
+        @Specialization
+        Object doClosure(
+            CraterClosure callee,
+            Object continuationFrame,
+            Object[] arguments,
+            @Cached CraterClosure.InvokeNode closureInvokeNode
+        ) {
+            return closureInvokeNode.execute(callee, continuationFrame, arguments);
+        }
+
+        @Specialization
+        Object doBuiltin(
+            CraterBuiltin callee,
+            Object continuationFrame,
+            Object[] arguments,
+            @Cached CraterBuiltin.InvokeNode builtinInvokeNode
+        ) {
+            return builtinInvokeNode.execute(callee, continuationFrame, arguments);
+        }
     }
 }
--- language/src/main/java/org/craterlang/language/nodes/CraterTailCallException.java
+++ language/src/main/java/org/craterlang/language/nodes/CraterTailCallException.java
@@ -1,22 +1,25 @@
-package org.craterlang.language.runtime;
+package org.craterlang.language.nodes;
 
-import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.nodes.ControlFlowException;
 
 public final class CraterTailCallException extends ControlFlowException {
-    private final CallTarget callee;
+    private final Object callee;
+    private final Object continuationFrame;
     @CompilationFinal(dimensions = 1) private final Object[] arguments;
 
-    public CraterTailCallException(CallTarget callee, Object[] arguments) {
-        assert callee != null;
-        assert arguments != null;
+    public CraterTailCallException(Object callee, Object continuationFrame, Object[] arguments) {
         this.callee = callee;
+        this.continuationFrame = continuationFrame;
         this.arguments = arguments;
     }
 
     public Object getCallee() {
         return callee;
+    }
+
+    public Object getContinuationFrame() {
+        return continuationFrame;
     }
 
     public Object[] getArguments() {
