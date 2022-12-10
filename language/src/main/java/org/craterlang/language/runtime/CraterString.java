571103f09af7eef2c558fbdd30062feacb209b58
Lucas Myers <lucascharlesmyers@gmail.com>
2022-12-10 12:08:59 -0500

    WIP

--- language/src/main/java/org/craterlang/language/runtime/CraterString.java
+++ language/src/main/java/org/craterlang/language/runtime/CraterString.java
@@ -3,25 +3,26 @@ package org.craterlang.language.runtime;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Bind;
 import com.oracle.truffle.api.dsl.Cached;
-import com.oracle.truffle.api.dsl.Cached.Shared;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.GeneratePackagePrivate;
 import com.oracle.truffle.api.dsl.GenerateUncached;
-import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.oracle.truffle.api.profiles.BranchProfile;
+import com.oracle.truffle.api.profiles.ConditionProfile;
 import com.oracle.truffle.api.profiles.IntValueProfile;
 import org.craterlang.language.CraterNode;
-import org.craterlang.language.nodes.CraterPathProfileNode;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import static com.oracle.truffle.api.CompilerDirectives.castExact;
 import static com.oracle.truffle.api.CompilerDirectives.isExact;
+import static java.lang.Double.isNaN;
 import static java.lang.Double.longBitsToDouble;
 import static java.lang.System.arraycopy;
+import static org.craterlang.language.runtime.CraterMath.hasExactLongValue;
 
 public final class CraterString implements TruffleObject {
     private Object objectData;
@@ -30,11 +31,30 @@ public final class CraterString implements TruffleObject {
     private static final Object TAG_LAZY_LONG = new Object();
     private static final Object TAG_LAZY_DOUBLE = new Object();
 
-    private static final byte TAG_INTERNED = 0;
-    private static final byte TAG_DYNAMIC = 1;
-    private static final byte TAG_LAZY_CONCAT = 2;
-    private static final byte TAG_LAZY_REPEAT = 3;
-    private static final byte TAG_LAZY_REVERSE = 4;
+    private static final byte TAG_IMMEDIATE = 0;
+    private static final byte TAG_LAZY_CONCAT = 1;
+    private static final byte TAG_LAZY_REPEAT = 2;
+    private static final byte TAG_LAZY_REVERSE = 3;
+
+    @TruffleBoundary
+    public void forceUncached() {
+        ForceNode.getUncached().execute(this);
+    }
+
+    @TruffleBoundary(allowInlining = true)
+    @Override public boolean equals(Object obj) {
+        if (!(obj instanceof CraterString other)) {
+            return false;
+        }
+
+        return EqualsNode.getUncached().execute(this, other);
+    }
+
+    @TruffleBoundary
+    @Override public String toString() {
+        forceUncached();
+        return new String(getImmediateBytes(), StandardCharsets.UTF_8);
+    }
 
     boolean isLazyLong() {
         return objectData == TAG_LAZY_LONG;
@@ -54,57 +74,38 @@ public final class CraterString implements TruffleObject {
         return longBitsToDouble(primitiveData);
     }
 
-    boolean isInterned() {
-        return objectData instanceof byte[] && (byte) primitiveData == TAG_INTERNED;
+    boolean isImmediate() {
+        return objectData instanceof byte[] && (byte) primitiveData == TAG_IMMEDIATE;
     }
 
-    byte[] getInternedBytes() {
-        assert isInterned();
+    byte[] getImmediateBytes() {
+        assert isImmediate();
         return (byte[]) objectData;
     }
 
-    int getInternedLength() {
-        assert isInterned();
-        return getInternedBytes().length;
+    int getImmediateLength() {
+        assert isImmediate();
+        return getImmediateBytes().length;
     }
 
-    int getInternedHashCode() {
-        assert isInterned();
-        return (int) (primitiveData >>> 8);
-    }
-
-    boolean isDynamic() {
-        return objectData instanceof byte[] && (byte) primitiveData == TAG_DYNAMIC;
-    }
-
-    byte[] getDynamicBytes() {
-        assert isDynamic();
-        return (byte[]) objectData;
-    }
-
-    int getDynamicLength() {
-        assert isDynamic();
-        return getDynamicBytes().length;
-    }
-
-    boolean hasDynamicHashCode() {
-        assert isDynamic();
+    boolean hasImmediateHashCode() {
+        assert isImmediate();
         return primitiveData >= 0;
     }
 
-    int getDynamicHashCode() {
-        assert hasDynamicHashCode();
+    int getImmediateHashCode() {
+        assert hasImmediateHashCode();
         return (int) (primitiveData >>> 8);
     }
 
-    void setDynamicHashCode(int value) {
-        assert isDynamic();
-        primitiveData = (Integer.toUnsignedLong(value) << 8) | TAG_DYNAMIC;
+    void setImmediateHashCode(int value) {
+        assert isImmediate();
+        primitiveData = (Integer.toUnsignedLong(value) << 8) | TAG_IMMEDIATE;
     }
 
-    void setDynamic(byte[] bytes) {
+    void setImmediate(byte[] bytes) {
         objectData = bytes;
-        primitiveData = Long.MAX_VALUE | TAG_DYNAMIC;
+        primitiveData = Long.MAX_VALUE | TAG_IMMEDIATE;
     }
 
     boolean isLazyConcat() {
@@ -135,6 +136,11 @@ public final class CraterString implements TruffleObject {
         return (int) (primitiveData >>> 8);
     }
 
+    CraterString getLazyRepeatChild() {
+        assert isLazyRepeat();
+        return (CraterString) objectData;
+    }
+
     boolean isLazyReverse() {
         return objectData instanceof byte[] && (byte) primitiveData == TAG_LAZY_REVERSE;
     }
@@ -142,6 +148,11 @@ public final class CraterString implements TruffleObject {
     int getLazyReverseLength() {
         assert isLazyReverse();
         return (int) (primitiveData >>> 8);
+    }
+
+    CraterString getLazyReverseChild() {
+        assert isLazyReverse();
+        return (CraterString) objectData;
     }
 
     @GenerateUncached
@@ -157,14 +168,9 @@ public final class CraterString implements TruffleObject {
             return CraterStringFactory.LengthNodeGen.getUncached();
         }
 
-        @Specialization(guards = "string.isInterned()")
-        int doInterned(CraterString string) {
-            return string.getInternedLength();
-        }
-
-        @Specialization(guards = "string.isDynamic()")
-        int doDynamic(CraterString string) {
-            return string.getDynamicLength();
+        @Specialization(guards = "string.isImmediate()")
+        int doImmediate(CraterString string) {
+            return string.getImmediateLength();
         }
 
         @Specialization(guards = "string.isLazyConcat()")
@@ -197,23 +203,15 @@ public final class CraterString implements TruffleObject {
         }
 
         @Specialization
-        int doExecute(CraterString string, @Cached ForceNode forceNode, @Cached CraterPathProfileNode pathProfileNode) {
+        int doExecute(CraterString string, @Cached ForceNode forceNode, @Cached BranchProfile missingHashCodeProfile) {
             forceNode.execute(string);
 
-            if (string.isInterned()) {
-                pathProfileNode.execute(0);
-                return string.getInternedHashCode();
+            if (!string.hasImmediateHashCode()) {
+                missingHashCodeProfile.enter();
+                string.setImmediateHashCode(hashCodeBoundary(string.getImmediateBytes()));
             }
 
-            pathProfileNode.execute(1);
-
-            if (!string.hasDynamicHashCode()) {
-                pathProfileNode.execute(2);
-                var hashCode = hashCodeBoundary(string.getDynamicBytes());
-                string.setDynamicHashCode(hashCode);
-            }
-
-            return string.getDynamicHashCode();
+            return string.getImmediateHashCode();
         }
 
         @TruffleBoundary(allowInlining = true)
@@ -235,11 +233,8 @@ public final class CraterString implements TruffleObject {
             return CraterStringFactory.ForceNodeGen.getUncached();
         }
 
-        @Specialization(guards = "string.isInterned()")
-        void doInterned(CraterString string) {}
-
-        @Specialization(guards = "string.isDynamic()")
-        void doDynamic(CraterString string) {}
+        @Specialization(guards = "string.isImmediate()")
+        void doImmediate(CraterString string) {}
 
         @Fallback
         void doOther(
@@ -252,7 +247,7 @@ public final class CraterString implements TruffleObject {
             var bytes = new byte[length];
             var writtenLength = appendSubstringNode.execute(string, bytes, 0);
             assert writtenLength == length;
-            string.setDynamic(bytes);
+            string.setImmediate(bytes);
         }
     }
 
@@ -264,31 +259,29 @@ public final class CraterString implements TruffleObject {
             return CraterStringFactory.AppendSubstringNodeGen.create();
         }
 
-        @Specialization(guards = "string.isInterned()")
-        int doInterned(CraterString string, byte[] destination, int offset) {
-            var bytes = string.getInternedBytes();
-            arraycopy(bytes, 0, destination, offset, bytes.length);
-            return offset + bytes.length;
-        }
-
-        @Specialization(guards = "string.isDynamic()")
-        int doDynamic(CraterString string, byte[] destination, int offset) {
-            var bytes = string.getDynamicBytes();
+        @Specialization(guards = "string.isImmediate()")
+        int doImmediate(CraterString string, byte[] destination, int offset) {
+            var bytes = string.getImmediateBytes();
             arraycopy(bytes, 0, destination, offset, bytes.length);
             return offset + bytes.length;
         }
 
         @ExplodeLoop
-        @Specialization(guards = {"string.isLazyConcat()", "substrings.length == substringsLength"}, limit = "1")
+        @Specialization(
+            guards = {
+                "string.isLazyConcat()",
+                "substrings.length == appendSubstringNodes.length",
+            },
+            limit = "1"
+        )
         int doExplodedLazyConcat(
             CraterString string,
             byte[] destination,
             int offset,
             @Bind("string.getLazyConcatSubstrings()") CraterString[] substrings,
-            @Cached("substrings.length") int substringsLength,
-            @Cached("createAppendSubstringNodes(substringsLength)") AppendSubstringNode[] appendSubstringNodes
+            @Cached("createAppendSubstringNodes(substrings.length)") AppendSubstringNode[] appendSubstringNodes
         ) {
-            for (var i = 0; i < substringsLength; i++) {
+            for (var i = 0; i < appendSubstringNodes.length; i++) {
                 offset = appendSubstringNodes[i].execute(substrings[i], destination, offset);
             }
 
@@ -350,6 +343,161 @@ public final class CraterString implements TruffleObject {
         static AppendSubstringNode[] createAppendSubstringNodes(int length) {
             var nodes = new AppendSubstringNode[length];
             for (var i = 0; i < nodes.length; i++) nodes[i] = AppendSubstringNode.create();
+            return nodes;
+        }
+    }
+
+    @GenerateUncached
+    @GeneratePackagePrivate
+    public static abstract class EqualsNode extends CraterNode {
+        public abstract boolean execute(CraterString lhs, CraterString rhs);
+
+        public static EqualsNode create() {
+            return CraterStringFactory.EqualsNodeGen.create();
+        }
+
+        public static EqualsNode getUncached() {
+            return CraterStringFactory.EqualsNodeGen.getUncached();
+        }
+
+        @Specialization(guards = "lhs == rhs")
+        boolean doIdentical(CraterString lhs, CraterString rhs) {
+            return true;
+        }
+
+        @Fallback
+        boolean doDeep(CraterString lhs, CraterString rhs, @Cached DeepEqualsNode deepEqualsNode) {
+            return deepEqualsNode.execute(lhs, rhs);
+        }
+    }
+
+    @GenerateUncached
+    static abstract class DeepEqualsNode extends CraterNode {
+        abstract boolean execute(CraterString lhs, CraterString rhs);
+
+        @Specialization(guards = {"lhs.isLazyLong()", "rhs.isLazyLong()"})
+        boolean doLazyLongs(CraterString lhs, CraterString rhs) {
+            return lhs.getLazyLongValue() == rhs.getLazyLongValue();
+        }
+
+        @Specialization(guards = {"lhs.isLazyDouble()", "rhs.isLazyDouble()"})
+        boolean doLazyDoubles(CraterString lhs, CraterString rhs) {
+            if (isNaN(lhs.getLazyDoubleValue()) && isNaN(rhs.getLazyDoubleValue())) {
+                return true;
+            }
+
+            return lhs.getLazyDoubleValue() == rhs.getLazyDoubleValue();
+        }
+
+        @Specialization(guards = {"lhs.isLazyLong()", "rhs.isLazyDouble()"})
+        boolean doLazyLongAndLazyDouble(CraterString lhs, CraterString rhs) {
+            return hasExactLongValue(rhs.getLazyDoubleValue())
+                && lhs.getLazyLongValue() == (long) rhs.getLazyDoubleValue();
+        }
+
+        @Specialization(guards = {"lhs.isLazyDouble()", "rhs.isLazyLong()"})
+        boolean doLazyDoubleAndLazyLong(CraterString lhs, CraterString rhs) {
+            return hasExactLongValue(lhs.getLazyDoubleValue())
+                && (long) lhs.getLazyDoubleValue() == rhs.getLazyLongValue();
+        }
+
+        @ExplodeLoop
+        @Specialization(
+            guards = {
+                "lhs.isLazyConcat()",
+                "rhs.isLazyConcat()",
+                "lhs.getLazyConcatLength() == rhs.getLazyConcatLength()",
+                "lhsSubstrings.length == rhsSubstrings.length",
+                "lhsSubstrings.length == innerEqualsNodes.length",
+            },
+            limit = "1"
+        )
+        boolean doExplodedLazyConcats(
+            CraterString lhs,
+            CraterString rhs,
+            @Bind("lhs.getLazyConcatSubstrings()") CraterString[] lhsSubstrings,
+            @Bind("rhs.getLazyConcatSubstrings()") CraterString[] rhsSubstrings,
+            @Cached("createInnerEqualsNodes(lhsSubstrings.length)") EqualsNode[] innerEqualsNodes
+        ) {
+            for (var i = 0; i < innerEqualsNodes.length; i++) {
+                if (!innerEqualsNodes[i].execute(lhsSubstrings[i], rhsSubstrings[i])) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        @Specialization(
+            guards = {
+                "lhs.isLazyConcat()",
+                "rhs.isLazyConcat()",
+                "lhs.getLazyConcatLength() == rhs.getLazyConcatLength()",
+                "lhsSubstrings.length == rhsSubstrings.length"
+            },
+            replaces = "doExplodedLazyConcats"
+        )
+        boolean doLazyConcats(
+            CraterString lhs,
+            CraterString rhs,
+            @Bind("lhs.getLazyConcatSubstrings()") CraterString[] lhsSubstrings,
+            @Bind("rhs.getLazyConcatSubstrings()") CraterString[] rhsSubstrings,
+            @Cached EqualsNode innerEqualsNode
+        ) {
+            for (var i = 0; i < lhsSubstrings.length; i++) {
+                if (!innerEqualsNode.execute(lhsSubstrings[i], rhsSubstrings[i])) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        @Specialization(guards = {
+            "lhs.isLazyRepeat()",
+            "rhs.isLazyRepeat()",
+            "lhs.getLazyRepeatLength() == rhs.getLazyRepeatLength()",
+            "lhs.getLazyRepeatCount() == rhs.getLazyRepeatCount()",
+        })
+        boolean doLazyRepeats(CraterString lhs, CraterString rhs, @Cached EqualsNode innerEqualsNode) {
+            return innerEqualsNode.execute(lhs.getLazyRepeatChild(), rhs.getLazyRepeatChild());
+        }
+
+        @Specialization(guards = {
+            "lhs.isLazyReverse()",
+            "rhs.isLazyReverse()",
+            "lhs.getLazyReverseLength() == rhs.getLazyReverseLength()"
+        })
+        boolean doLazyReversals(CraterString lhs, CraterString rhs, @Cached EqualsNode innerEqualsNode) {
+            return innerEqualsNode.execute(lhs.getLazyReverseChild(), rhs.getLazyRepeatChild());
+        }
+
+        @Fallback
+        boolean doBytewise(
+            CraterString lhs,
+            CraterString rhs,
+            @Cached LengthNode lhsLengthNode,
+            @Cached LengthNode rhsLengthNode,
+            @Cached ConditionProfile lengthMismatchProfile,
+            @Cached ForceNode lhsForceNode,
+            @Cached ForceNode rhsForceNode
+        ) {
+            var lhsLength = lhsLengthNode.execute(lhs);
+            var rhsLength = rhsLengthNode.execute(rhs);
+
+            if (lengthMismatchProfile.profile(lhsLength != rhsLength)) {
+                return false;
+            }
+
+            lhsForceNode.execute(lhs);
+            rhsForceNode.execute(rhs);
+
+            return Arrays.equals(lhs.getImmediateBytes(), rhs.getImmediateBytes());
+        }
+
+        static EqualsNode[] createInnerEqualsNodes(int length) {
+            var nodes = new EqualsNode[length];
+            for (var i = 0; i < nodes.length; i++) nodes[i] = EqualsNode.create();
             return nodes;
         }
     }
